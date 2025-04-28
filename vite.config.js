55f41efa2bebf7f022c5d6eb8a74ba98deb336d6
Praganya Mittal <praganyamittal04@gmail.com>
2025-04-29 00:58:44 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
+    build: {
+        outDir: 'dist', // <-- force it to generate a 'dist' folder
+    },
 });
